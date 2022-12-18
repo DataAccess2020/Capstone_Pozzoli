c0806dd3119c1bbaccdde1361ffc96fc07fb0d0b
cristina-pozzoli <cristina.pozzoli@icloud.com>
2022-12-18 10:58:16 +0100

    test to see if it is possible save the dataframe keeping the correct dates

--- Scripts/04_Dataframe_Saving_prova.R
+++ Scripts/04_Dataframe_Saving_prova.R
@@ -7,5 +7,13 @@ write.csv(tweets_Azione2, "Azione_tweets.csv", row.names = F, fileEncoding = "UT
 tweets_FDI2 <- sapply(tweets_FDI, as.character)
 write.csv(tweets_FDI2, "FDI_tweets.csv", row.names = F, fileEncoding = "UTF-8")
 
+tweets_FI2 <- sapply(tweets_FI, as.character)
+write.csv(tweets_FI2, "FI_tweets.csv", row.names = F, fileEncoding = "UTF-8")
+
+tweets_IV2 <- sapply(tweets_IV, as.character)
+write.csv(tweets_IV2, "IV_tweets.csv", row.names = F, fileEncoding = "UTF-8")
+
+tweets_lega2 <- sapply(tweets_lega, as.character)
+write.csv(tweets_lega2, "lega_tweets.csv", row.names = F, fileEncoding = "UTF-8")
 
 
