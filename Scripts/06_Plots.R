72c0e8d49cc50f229e9d1d5a76da167327764fc5
cristina-pozzoli <cristina.pozzoli@icloud.com>
2022-12-22 15:36:55 +0100

    Saves plots

--- Scripts/06_Plots.R
+++ Scripts/06_Plots.R
@@ -69,7 +69,7 @@ tweets_frequency %>%
 
 tweets_frequency %>% 
   ggplot(aes(x = Parties, y=(tweets_qatar)/sum(tweets_qatar))) +
-  geom_col(alpha=0.8, fill="green") +
+  geom_col(alpha=0.5, fill="green") +
   ylab("Tweets about Qatar") +
   xlab("Parties") +
   ggtitle("Distribution of tweets about Qatar for each party") +
@@ -80,7 +80,7 @@ tweets_frequency %>%
   
 tweets_frequency %>% 
   ggplot(aes(x = Parties, y=(tweets_energia)/sum(tweets_energia))) +
-  geom_col(alpha=0.8, fill="red") +
+  geom_col(alpha=0.5, fill="red") +
   ylab("Tweets about Energetic crisis") +
   xlab("Parties") +
   ggtitle("Distribution of tweets about Energy crisis for each party") +
@@ -88,3 +88,51 @@ tweets_frequency %>%
   scale_x_discrete(labels = label_wrap(10)) + 
   theme_bw() +
   ggsave("Energycrisis_tweets_distribution.jpeg")
+
+# Now, I put the plots together to make the comparison easier
+# In order to do that I have to create some objects 
+
+library(ggpubr)
+
+a <- tweets_frequency %>% 
+  ggplot(aes(x = Parties, y=(tweets_iran)/sum(tweets_iran)))+
+  geom_col(alpha=0.8, fill="pink") +
+  ylab("Tweets about Iran") +
+  xlab("Parties") +
+  scale_y_continuous(labels = scales::percent)+
+  scale_x_discrete(labels = label_wrap(10)) +
+  theme_bw()
+
+
+b <- tweets_frequency %>% 
+  ggplot(aes(x = Parties, y=(tweets_ucraina)/sum(tweets_ucraina)))+
+  geom_col(alpha=0.5, fill="blue") +
+  ylab("Tweets about Ukraine") +
+  xlab("Parties") +
+  scale_y_continuous(labels = scales::percent)+
+  scale_x_discrete(labels = label_wrap(10))  +
+  theme_bw()
+
+
+c <-tweets_frequency %>% 
+  ggplot(aes(x = Parties, y=(tweets_qatar)/sum(tweets_qatar))) +
+  geom_col(alpha=0.5, fill="green") +
+  ylab("Tweets about Qatar") +
+  xlab("Parties") +
+  scale_x_discrete(labels = label_wrap(10))  + 
+  scale_y_continuous(labels = scales::percent) +
+  theme_bw()
+
+d <- tweets_frequency %>% 
+  ggplot(aes(x = Parties, y=(tweets_energia)/sum(tweets_energia))) +
+  geom_col(alpha=0.5, fill="red") +
+  ylab("Tweets about Energetic crisis") +
+  xlab("Parties") +
+  scale_y_continuous(labels = scales::percent) +
+  scale_x_discrete(labels = label_wrap(10)) + 
+  theme_bw()
+
+ggarrange(a, b, c, d, 
+          ncol = 2, nrow = 2) +
+  ggsave("Finalplot.jpeg", width = 25, height = 20, units = "cm")
+
